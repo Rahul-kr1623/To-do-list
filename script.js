c0747f1fb5e6681196d2c3aa372a4a4917b88b9a
Rahul Kumar <rahulkr23082006@gmail.com>
2026-02-20 18:10:11 +0530

    formatted

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 const inputBox = document.getElementById("input-box");
-        // const addButton = document.querySelector(".row button");
-        const listContainer = document.getElementById("list-container");
+// const addButton = document.querySelector(".row button");
+const listContainer = document.getElementById("list-container");
 
-        // Function to add a new task
-        function addTask() {
-            const taskText = inputBox.value.trim();
-            if (taskText === "") {
-                alert("You must write something!");
-                return;
-            }
-            else {
-                const li = document.createElement("li");
-                li.innerHTML = taskText;
-                listContainer.appendChild(li);
-                let span = document.createElement("span");
-                span.innerHTML = "\u00d7"; // Unicode for multiplication sign (×)
-                li.appendChild(span);
-            }
-            inputBox.value =""; // Clear the input box
-            saveData();
+// Function to add a new task
+function addTask() {
+    const taskText = inputBox.value.trim();
+    if (taskText === "") {
+        alert("You must write something!");
+        return;
+    }
+    else {
+        const li = document.createElement("li");
+        li.innerHTML = taskText;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; // Unicode for multiplication sign (×)
+        li.appendChild(span);
+    }
+    inputBox.value = ""; // Clear the input box
+    saveData();
 
-        }
+}
 
-         listContainer.addEventListener("click", function(e){
-                if(e.target.tagName === "LI"){
-                    e.target.classList.toggle("checked");
-                    saveData()
-                }
-                else if(e.target.tagName === "SPAN"){
-                    e.target.parentElement.remove();
-                    saveData()
-                }
-            }, false);
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
 
-            function saveData(){
-                localStorage.setItem("data", listContainer.innerHTML);
-            }
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
 
-            function showTask(){
-                listContainer.innerHTML = localStorage.getItem("data");
-            }
-            showTask();
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
